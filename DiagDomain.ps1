2069c89387a22e4d282472d5d60528b1bea5921b
James A. Wall <g60wall@gmail.com>
2015-08-17 08:11:26 -0400

    Basic Domain  Reporting
    mac address and ipv4 address and Operating system

---
+++ DiagDomain.ps1
@@ -0,0 +1,36 @@
+﻿$Header = @"
+<style>
+TABLE {border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}
+TH {border-width: 1px;padding: 3px;border-style: solid;border-color: black;background-color: #6495ED;}
+TD {border-width: 1px;padding: 3px;border-style: solid;border-color: black;}
+TR:Nth-Child(Even) {Background-Color: #dddddd;}
+TR:Hover TD {Background-Color: #C1D5F8;}
+</style>
+<title>
+Domain Computer Details
+</title>
+"@
+
+$ADComputerProperties = @(`
+"Operatingsystem",
+"OperatingSystemServicePack",
+"Created",
+"Enabled",
+"LastLogonDate",
+"IPv4Address",
+"CanonicalName"
+)
+ 
+$SelectADComputerProperties = @(`
+"Name",
+"OperatingSystem",
+"OperatingSystemServicePack",
+"Created",
+"Enabled",
+"LastLogonDate",
+"IPv4Address",
+"CanonicalName"
+)
+
+Get-ADComputer -Filter * -Properties $ADComputerProperties  |  select $SelectADComputerProperties | ConvertTo-Html -Head $Header  | Out-File \\nas\it\james\OUTFILE\DIAGdomain.html
+
---
+++ Inventory.ps1
@@ -0,0 +1,70 @@
+﻿
+
+$Header = @"
+<style>
+TABLE {border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}
+TH {border-width: 1px;padding: 3px;border-style: solid;border-color: black;background-color: #6495ED;}
+TD {border-width: 1px;padding: 3px;border-style: solid;border-color: black;}
+TR:Nth-Child(Even) {Background-Color: #dddddd;}
+TR:Hover TD {Background-Color: #C1D5F8;}
+</style>
+<title>
+DNS Phonebook By Mac and Hostname
+</title>
+"@
+
+
+$today = Get-Date
+$cutoffdate = $today.AddDays(-15)
+
+Get-ADComputer  -Properties * -Filter {LastLogonDate -gt $cutoffdate}|Select -Expand DNSHostName  | out-file C:\All-Computers.txt
+$ComputerList = get-content "C:\All-Computers.txt"
+$Amount = $ComputerList.count
+$a=0
+
+foreach ($hosts in $ComputerList) 
+        {
+                        $Ping = Test-Path "\\$hosts\C$" -ErrorAction SilentlyContinue
+                        if ($Ping -eq "True") 
+                           {
+                                echo $hosts >> "C:\Online-Computers.txt"
+                                  $a++
+                                    Write-Progress -Activity "Working..." -CurrentOperation "$a complete of $Amount"  -Status "Please wait testing connections" 
+
+                                
+                           }
+
+        }
+
+$allhost = get-content "C:\Online-Computers.txt"
+"Hostname,MAC Address,Serial Number" >> C:\Inventory.csv
+$a=0
+$OnlineAmount = $allhost.count
+
+foreach ($computer in $allhost) {
+
+$Networks = Get-WmiObject Win32_NetworkAdapterConfiguration -ComputerName $computer | ? {$_.IPEnabled}
+   foreach ($Network in $Networks) {
+    $IsDHCPEnabled = $false
+    If($Network.DHCPEnabled) {
+     $IsDHCPEnabled = $true
+         }
+          $mac = $Network.MACAddress
+         }
+
+      $enclosure = Get-WmiObject -Class win32_systemenclosure -ComputerName $computer
+        $serial = $enclosure.SerialNumber
+          $output = $computer + "," + $mac + "," + $serial
+            $output >> C:\Inventory.csv
+               $a++
+                    
+                         
+          Write-Progress -Activity "Working..." -CurrentOperation "$a complete of $OnlineAmount"  -Status "Please wait testing connections"
+    }
+
+
+        Del "C:\Online-Computers.txt"
+           Del "C:\All-Computers.txt"
+
+          Import-Csv C:\Inventory.csv | ConvertTo-Html -Head $Header | Out-File \\nas\it\james\OUTFILE\Inventory.html
+
