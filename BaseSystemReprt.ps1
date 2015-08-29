6cfaf3c124c2fa2bd654253c441efc404bf4eaae
James A. Wall <g60wall@gmail.com>
2015-08-28 22:03:31 -0400

    Home Brewed Quick Access
    Plan on making a function

---
+++ BaseSystemReprt.ps1
@@ -0,0 +1,109 @@
+﻿$Header = @"
+<style>
+TABLE {border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}
+TH {border-width: 1px;padding: 3px;border-style: solid;border-color: black;background-color: #6495ED;}
+TD {border-width: 1px;padding: 3px;border-style: solid;border-color: black;}
+TR:Nth-Child(Even) {Background-Color: #dddddd;}
+TR:Hover TD {Background-Color: #C1D5F8;}
+</style>
+<title>
+$Title
+</title>
+"@
+$computername = Read-Host "Input Computer Name"
+
+function Get-InfoOS {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True)][string]$ComputerName
+    )
+    $os = Get-WmiObject -class Win32_OperatingSystem -ComputerName $ComputerName
+    $props = @{'OSVersion'=$os.version
+               'SPVersion'=$os.servicepackmajorversion;
+               'OSBuild'=$os.buildnumber}
+    New-Object -TypeName PSObject -Property $props
+}
+
+function Get-InfoCompSystem {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True)][string]$ComputerName
+    )
+    $cs = Get-WmiObject -class Win32_ComputerSystem -ComputerName $ComputerName
+    $props = @{'Model'=$cs.model;
+               'Manufacturer'=$cs.manufacturer;
+               'RAM (GB)'="{0:N2}" -f ($cs.totalphysicalmemory / 1GB);
+               'Sockets'=$cs.numberofprocessors;
+               'Cores'=$cs.numberoflogicalprocessors}
+    New-Object -TypeName PSObject -Property $props
+}
+
+function Get-InfoBadService {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True)][string]$ComputerName
+    )
+    $svcs = Get-WmiObject -class Win32_Service -ComputerName $ComputerName `
+           -Filter "StartMode='Auto' AND State<>'Running'"
+    foreach ($svc in $svcs) {
+        $props = @{'ServiceName'=$svc.name;
+                   'LogonAccount'=$svc.startname;
+                   'DisplayName'=$svc.displayname}
+        New-Object -TypeName PSObject -Property $props
+    }
+}
+
+function Get-InfoProc {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True)][string]$ComputerName
+    )
+    $procs = Get-WmiObject -class Win32_Process -ComputerName $ComputerName
+    foreach ($proc in $procs) { 
+        $props = @{'ProcName'=$proc.name;
+                   'Executable'=$proc.ExecutablePath}
+        New-Object -TypeName PSObject -Property $props
+    }
+}
+
+function Get-InfoNIC {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True)][string]$ComputerName
+    )
+    $nics = Get-WmiObject -class Win32_NetworkAdapter -ComputerName $ComputerName `
+           -Filter "PhysicalAdapter=True"
+    foreach ($nic in $nics) {      
+        $props = @{'NICName'=$nic.servicename;
+                   'Speed'=$nic.speed / 1MB -as [int];
+                   'Manufacturer'=$nic.manufacturer;
+                   'MACAddress'=$nic.macaddress}
+        New-Object -TypeName PSObject -Property $props
+    }
+}
+
+function Get-InfoDisk {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True)][string]$ComputerName
+    )
+    $drives = Get-WmiObject -class Win32_LogicalDisk -ComputerName $ComputerName `
+           -Filter "DriveType=3"
+    foreach ($drive in $drives) {      
+        $props = @{'Drive'=$drive.DeviceID;
+                   'Size'=$drive.size / 1GB -as [int];
+                   'Free'="{0:N2}" -f ($drive.freespace / 1GB);
+                   'FreePct'=$drive.freespace / $drive.size * 100 -as [int]}
+        New-Object -TypeName PSObject -Property $props 
+    }
+}
+
+
+$a = Get-InfoOS -ComputerName $computername | ConvertTo-Html -Fragment
+$b = Get-InfoCompSystem -ComputerName $computername | ConvertTo-Html -Fragment
+$c = Get-InfoBadService -ComputerName $computername | ConvertTo-html -Fragment
+$f = Get-InfoProc -ComputerName $computername | ConvertTo-Html -Fragment
+$e = Get-InfoNIC -ComputerName $computername | ConvertTo-Html -PreContent "Network Card Info" -Fragment
+$d = Get-InfoDisk -ComputerName $computername | ConvertTo-Html -PreContent DiskInfo -Fragment
+
+ConvertTo-Html -head $header -Body "$a,$b,$c,$d,$e,$f" | Out-File c:\HostInfo.html
