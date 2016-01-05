00a7206548971acfdecaaea7279e1bb83dbd7e57
James A. Wall <g60wall@gmail.com>
2016-01-04 23:57:03 -0500

    Get-Weather
    Code not function

---
+++ Get-Weather.ps1
@@ -0,0 +1,24 @@
+﻿$City = "wanaque"
+$country = "us"
+
+
+$api = "692447105f67a34c06d1cbeab56b17c5"
+
+[xml]$wr = Invoke-WebRequest "api.openweathermap.org/data/2.5/weather?q=$City,$country&APPID=$api&mode=xml"
+$data = $wr.current
+
+$weather = [PSCustomObject]@{
+City       = $data.city.name + "," + $data.city.country
+Weather    =  $data.weather.value
+TempNOW    = ([math]::Round(($data.temperature.value - 273.15)*9/5+32))
+tempMAX    = ([math]::Round(($data.temperature.max - 273.15)*9/5+32))
+TempMIN    = ([math]::Round(($data.temperature.min - 273.15)*9/5+32))
+humidity   =  $data.humidity.value + $data.humidity.unit
+Clouds     =  $data.clouds.name
+Rain       =  $data.precipitation.mode
+Wind       =  $data.wind.Value
+pressure   =  $data.pressure.value
+}
+
+$weather
+
