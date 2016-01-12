dc9d455beec6edae34778b0a1eb7ef9d4cb5aaa5
James A. Wall <g60wall@gmail.com>
2016-01-11 23:11:56 -0500

    Gui-Builder
    boom

---
+++ GUI-Builder.ps1
@@ -0,0 +1,93 @@
+﻿
+$inputXML = @"
+<Window x:Class="WpfApplication2.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:local="clr-namespace:WpfApplication2"
+        mc:Ignorable="d"
+        Title="Braen Stone Query" Height="416.794" Width="598.474" Topmost="True">
+    <Grid Margin="0,0,45,0">
+        <Image x:Name="image" HorizontalAlignment="Left" Height="100" Margin="24,28,0,0" VerticalAlignment="Top" Width="100" Source="C:\Users\Stephen\Dropbox\Docs\blog\foxdeploy favicon.png"/>
+        <TextBlock x:Name="textBlock" HorizontalAlignment="Left" Height="100" Margin="174,28,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="282" FontSize="16"><Run Text="Input Computer Name"/><InlineUIContainer>
+                <TextBlock x:Name="textBlock1" TextWrapping="Wrap" Text="TextBlock"/> 
+            </InlineUIContainer></TextBlock>
+        <Button x:Name="button" Content="get-DiskInfo" HorizontalAlignment="Left" Height="35" Margin="393,144,0,0" VerticalAlignment="Top" Width="121" FontSize="18.667"/>
+        <TextBox x:Name="textBox" HorizontalAlignment="Left" Height="35" Margin="186,144,0,0" TextWrapping="Wrap" Text="TextBox" VerticalAlignment="Top" Width="168" FontSize="16"/>
+        <Label x:Name="label" Content="ComputerName" HorizontalAlignment="Left" Height="46" Margin="24,144,0,0" VerticalAlignment="Top" Width="138" FontSize="16"/>
+        <ListView x:Name="listView" HorizontalAlignment="Left" Height="156" Margin="24,195,0,0" VerticalAlignment="Top" Width="511" FontSize="16">
+            <ListView.View>
+                <GridView>
+                    <GridViewColumn Header="Drive Letter" DisplayMemberBinding ="{Binding 'Drive Letter'}" Width="120"/>
+                    <GridViewColumn Header="Drive Label" DisplayMemberBinding ="{Binding 'Drive Label'}" Width="120"/>
+                    <GridViewColumn Header="Size(GB)" DisplayMemberBinding ="{Binding Size(MB)}" Width="120"/>
+                    <GridViewColumn Header="FreeSpace%" DisplayMemberBinding ="{Binding FreeSpace%}" Width="120"/>
+                </GridView>
+            </ListView.View>
+        </ListView>
+ 
+ 
+    </Grid>
+</Window>
+ 
+"@       
+ 
+$inputXML = $inputXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N'  -replace '^<Win.*', '<Window'
+ 
+ 
+[void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
+[xml]$XAML = $inputXML
+#Read XAML
+ 
+    $reader=(New-Object System.Xml.XmlNodeReader $xaml) 
+  try{$Form=[Windows.Markup.XamlReader]::Load( $reader )}
+catch{Write-Host "Unable to load Windows.Markup.XamlReader. Double-check syntax and ensure .net is installed."}
+ 
+#===========================================================================
+# Store Form Objects In PowerShell
+#===========================================================================
+ 
+$xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name "WPF$($_.Name)" -Value $Form.FindName($_.Name)}
+ 
+Function Get-FormVariables{
+if ($global:ReadmeDisplay -ne $true){Write-host "If you need to reference this display again, run Get-FormVariables" -ForegroundColor Yellow;$global:ReadmeDisplay=$true}
+write-host "Found the following interactable elements from our form" -ForegroundColor Cyan
+get-variable WPF*
+}
+ 
+Get-FormVariables
+ 
+#===========================================================================
+# Actually make the objects work
+#===========================================================================
+ 
+ 
+ 
+Function Get-DiskInfo {
+param($computername =$env:COMPUTERNAME)
+ 
+Get-WMIObject Win32_logicaldisk -ComputerName $computername | Select-Object @{Name='ComputerName';Ex={$computername}},`
+                                                                    @{Name=‘Drive Letter‘;Expression={$_.DeviceID}},`
+                                                                    @{Name=‘Drive Label’;Expression={$_.VolumeName}},`
+                                                                    @{Name=‘Size(MB)’;Expression={[int]($_.Size / 1GB)}},`
+                                                                    @{Name=‘FreeSpace%’;Expression={[math]::Round($_.FreeSpace / $_.Size,2)*100}}
+                                                                 }
+                                                                  
+$WPFtextBox.Text = $env:COMPUTERNAME
+ 
+$WPFbutton.Add_Click({
+$WPFlistView.Items.Clear()
+start-sleep -Milliseconds 840
+Get-DiskInfo -computername $WPFtextBox.Text | % {$WPFlistView.AddChild($_)}
+})
+#Sample entry of how to add data to a field
+ 
+ 
+#$vmpicklistView.items.Add([pscustomobject]@{'VMName'=($_).Name;Status=$_.Status;Other="Yes"})
+ 
+#===========================================================================
+# Shows the form
+#===========================================================================
+write-host "To show the form, run the following" -ForegroundColor Cyan
+$Form.ShowDialog() | out-null
---
+++ getsynomn.ps1
@@ -0,0 +1,87 @@
+﻿Function Get-Synonym()
+{
+# Parameter Definition
+
+Param(
+    [Parameter(Mandatory=$true)]
+    [ValidateScript({
+      
+      If($_ -match "^[a-zA-Z]+$"){
+      $True
+      }
+      else{
+      Throw "$_ is not a valid Word or contains something beyond Alphabets A-Z or a-z"
+      }
+    })
+    ] $Word
+     )
+
+Try
+{
+
+# Capturing XML data by requesting the URL
+
+    $webpage = Invoke-WebRequest -Uri "http://thesaurus.altervista.org/thesaurus/v1?word=$Word&key=qFTNpXBBp8xiNR0LdGCt&language=en_US" -UseBasicParsing -ErrorVariable ErrVar
+    
+    $Word = (Get-Culture).textinfo.totitlecase($Word)
+    $content = $webpage.Content
+    
+# Data Mining
+
+    $Category = (Select-Xml -content $content -xpath '//list/category').node.InnerText
+    $Category = $Category | %{$str=$_;(Get-Culture).textinfo.totitlecase($str.Substring(1,$str.Length-2))}
+    $Data = ((Select-Xml -content $content -xpath '//list/synonyms').node.InnerText)
+    
+       $Object = @()
+    
+    if($Category.count -gt 1)
+    {
+        For($i=0;$i -lt $($Category.Count);$i++)
+        {
+        $synonyms = (Get-Culture).textinfo.totitlecase($Data[$i]).Split('|')
+        
+            For($j=0;$j -lt $($synonyms.Count);$j++)
+            {
+        
+                $Object += New-Object psobject -Property @{Word=$Word;Category=Get-FullWord($Category[$i]);Synonym=$synonyms[$j]}
+            }
+        }
+        
+            Return $Object
+    }
+    else
+    {
+    
+    $synonyms = (Get-Culture).textinfo.totitlecase($Data).Split('|')
+        For($j=0;$j -lt $($synonyms.Count);$j++)
+        {
+    
+            $Object += New-Object psobject -Property @{Word=$Word;Category=Get-FullWord($Category);Synonym=$synonyms[$j]}
+    
+        }
+
+            Return $Object
+    }
+
+}
+catch
+{
+    Write-Host $_.exception.message -ForegroundColor Yellow
+}
+
+}
+
+
+# Function to Change Abbreviations to Full Forms
+Function Get-FullWord ($Abbreviation)
+{
+        $Table = @{
+                        "Adj"="Adjective"
+                        "Adv"="Adverb"
+                  }
+
+         $FullWord=$Table | %{ if($_.containskey($Abbreviation)){$_.get_item($Abbreviation)}else{$Abbreviation}  }
+
+         Return $FullWord
+}
+
